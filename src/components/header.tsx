d55c6701a0dd3c867859ed7140ead84d83442d7b
jonandgon <jonathan@puzzle.online>
2025-06-11 11:30:41 -0400

    Update header.tsx

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,10 +1,16 @@
-import { shortenAddress } from "@puzzlehq/sdk";
+import { shortenAddress, useDisconnect } from "@puzzlehq/sdk";
 
 function Header({ address }: { address: string | undefined }) {
+  const { disconnect, loading } = useDisconnect();
+
   return (
     <div className="fixed top-0 flex h-16 w-full items-center justify-between border-b bg-[#ffffff] px-8 dark:bg-[#242424]">
-      <span className="text-3xl font-bold">HoZK College Tour</span>
-      {address && <span className="text-m">{shortenAddress(address)}</span>}
+      <span className="text-3xl font-bold">Build a token</span>
+      {address && (
+        <button onClick={disconnect} disabled={loading}>
+          <span className="text-m">{shortenAddress(address)}</span>
+        </button>
+      )}
     </div>
   );
 }
